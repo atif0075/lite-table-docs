3335274355f9b20ebaa5b7170d36637359690fee
M Atif <chatif476@gmail.com>
2022-06-02 14:15:02 +0500

    optimized

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import App from "./App.vue";
 import "virtual:windi.css";
 import router from "./routes/routes";
 import VueCodeHighlight from "vue-code-highlight";
-import VueClickAway from "vue3-click-away";
 createApp(App)
-  .use(VueClickAway)
   .use(VueCodeHighlight)
   .use(router)
   .mount("#app");
