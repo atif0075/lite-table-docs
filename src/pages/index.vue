07803a819376830c5d84679049a4bfcc06a96664
M Atif <64689549+atif0075@users.noreply.github.com>
2022-06-15 02:13:39 -0700

    Styled dropdown of change language

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -32,13 +32,62 @@
             Source Code
           </a>
         </div>
-        <div class="flex flex-wrap justify-center gap-4 mt-8 text-black">
-          <select v-model="i18n.locale.value">
-            <option value="en">English</option>
-            <option value="jp">日本語</option>
-            <option value="tw">繁體中文</option>
-            <option value="cn">简体中文</option>
-          </select>
+          <div
+          class="absolute right-4 top-4 md:(right-12 top-12) inline-block text-left"
+        >
+          <div>
+            <button
+              @click="showLanguageSelector = !showLanguageSelector"
+              type="button"
+              class="inline-flex justify-center w-full rounded-md border border-dark shadow-sm px-4 py-2 bg-light-dark text-white text-sm font-medium focus:outline-none"
+            >
+              {{ currentLangName }}
+              <svg
+                class="-mr-1 ml-2 h-5 w-5"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+          <div
+            v-if="showLanguageSelector"
+            class="origin-top-right absolute right-0 mt-2 w-56 text-white rounded-md shadow-lg bg-light-dark focus:outline-none"
+          >
+            <div class="py-1 cursor-pointer">
+              <div
+                @click="changeLang('en', 'English')"
+                class="hover:bg-dark/40 block px-4 py-2 text-sm"
+              >
+                English
+              </div>
+              <div
+                @click="changeLang('jp', '日本語')"
+                class="hover:bg-dark/40 block px-4 py-2 text-sm"
+              >
+                日本語
+              </div>
+              <div
+                @click="changeLang('tw', '繁體中文')"
+                class="hover:bg-dark/40 block px-4 py-2 text-sm"
+              >
+                繁體中文
+              </div>
+              <div
+                @click="changeLang('cn', '简体中文')"
+                class="hover:bg-dark/40 block px-4 py-2 text-sm"
+              >
+                简体中文
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -48,4 +97,28 @@
 <script setup>
 import { useI18n } from "../plugins/i18n";
 const i18n = useI18n();
+let showLanguageSelector = ref(false);
+let currentLangName = ref("English");
+let changeLang = (lang, name) => {
+  i18n.locale.value = lang;
+  currentLangName.value = name;
+  showLanguageSelector.value = false;
+};
 </script>
+
+
+<style>
+::-webkit-scrollbar {
+  width: 8px;
+  height: 4px;
+}
+
+::-webkit-scrollbar-track {
+  @apply bg-dark;
+}
+
+::-webkit-scrollbar-thumb {
+  @apply bg-light-dark rounded-full;
+}
+</style>
+
