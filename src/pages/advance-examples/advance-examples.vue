12019185b2d2516222da2a4bdab14eebb6e29385
Lin Masahiro <k80092@gmail.com>
2022-07-04 11:38:59 +0900

    fixed #9

--- src/pages/advance-examples/advance-examples.vue
+++ src/pages/advance-examples/advance-examples.vue
@@ -2,47 +2,47 @@
   <section>
     <Header :title="i18n.$t('advance_examples')" />
     <section class="pt-20 px-3 pb-10">
-      <div id="customize-display-data">
+      <div id="customize-display-data" class="min-h-230">
         <h1 class="head">{{ i18n.$t("customize_display_data") }}</h1>
         <CusDisplayData class="pt-3" />
       </div>
-      <div id="filter">
+      <div id="filter" class="min-h-210">
         <h1 class="head">{{ i18n.$t("filter") }}</h1>
         <Filter class="pt-3" />
       </div>
-      <div id="asynchronous-filter">
+      <div id="asynchronous-filter" class="min-h-210">
         <h1 class="head">{{ i18n.$t("asynchronous_filter") }}</h1>
         <AsyncFilter class="pt-3" />
       </div>
-      <div id="filter-in-table">
+      <div id="filter-in-table" class="min-h-210">
         <h1 class="head">{{ i18n.$t("filter_in_table") }}</h1>
         <FilterInTable class="pt-3" />
       </div>
-      <div id="customize-style">
+      <div id="customize-style" class="min-h-230">
         <h1 class="head">{{ i18n.$t("customize_style") }}</h1>
         <CustomStyle class="pt-3" />
       </div>
-      <div id="customize-style-2">
+      <div id="customize-style-2" class="min-h-230">
         <h1 class="head">{{ i18n.$t("customize_style_2") }}</h1>
         <CustomStyle2 class="pt-3" />
       </div>
-      <div id="customize-message">
+      <div id="customize-message" class="min-h-200">
         <h1 class="head">{{ i18n.$t("customize_message") }}</h1>
         <CusMessage class="pt-3" />
       </div>
-      <div id="customize-page-dropdown">
+      <div id="customize-page-dropdown" class="min-h-250">
         <h1 class="head">{{ i18n.$t("customize_page_dropdown") }}</h1>
         <CusPageDrop class="pt-3" />
       </div>
-      <div id="catch-row-clicked">
+      <div id="catch-row-clicked" class="min-h-230">
         <h1 class="head">{{ i18n.$t("catch_row_clicked") }}</h1>
         <CatchRow class="pt-3" />
       </div>
-      <div id="fixed-first-column">
+      <div id="fixed-first-column" class="min-h-190">
         <h1 class="head">{{ i18n.$t("fixed_first_column") }}</h1>
         <FixedFirstColumn class="pt-3" />
       </div>
-      <div id="show-controls">
+      <div id="show-controls" class="min-h-100">
         <h1 class="head">{{ i18n.$t("show_controls") }}</h1>
         <ShowControls class="pt-3" />
       </div>
