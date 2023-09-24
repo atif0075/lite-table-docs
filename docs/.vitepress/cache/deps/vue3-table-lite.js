47d3c434a1bb829f70e785a0535994daf4db0613
M Atif <chatif476@gmail.com>
2023-09-24 15:50:49 +0500

    delete docs

--- docs/.vitepress/cache/deps/vue3-table-lite.js
+++
@@ -1,7 +0,0 @@
-// node_modules/vue3-table-lite/index.js
-import TableLite from "/Users/matifmehmood/Desktop/Code/lite-table-docs/node_modules/vue3-table-lite/src/components/TableLite.vue";
-var vue3_table_lite_default = TableLite;
-export {
-  vue3_table_lite_default as default
-};
-//# sourceMappingURL=vue3-table-lite.js.map
